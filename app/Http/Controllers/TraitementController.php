ba2cd42d9e6ae7272df85a95950801c940151ba5
elhoucine20 <elhoucinenaitbrahim@gmail.com>
2026-03-11 12:51:22 +0000

    add method de search

--- app/Http/Controllers/TraitementController.php
+++ app/Http/Controllers/TraitementController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTraitmentRequest;
 use App\Models\Traitement;
+use Exception;
 use Illuminate\Http\Request;
 
 class TraitementController extends Controller
 {
     //
-
     public function index(){
         $traitments = Traitement::paginate(2);
         return  $traitments;
@@ -37,4 +37,21 @@ class TraitementController extends Controller
       Traitement::destroy($id);
       return response()->json(['message'=>'deleted avec succes']);
     }
+
+public function search(Request $request){
+    try{
+        $query = Traitement::query();
+
+        if($request->has('description')){
+            $query->where('description','LIKE','%'.$request->description.'%');
+        }
+
+        $result = $query->paginate(2);
+
+        return response()->json($result,200);
+
+    }catch(\Exception $e){
+        return response()->json(['message' => $e->getMessage()]);
+    }
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,8 @@ use App\Http\Controllers\TraitementController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('traitments',TraitementController::class);
+Route::get('/traitment/search',[TraitementController::class,'search'])->name('search');
 
+Route::get('/test', function () {
+    return response()->json(['message' => 'API working']);
+});
