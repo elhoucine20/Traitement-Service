a47c68cbe97df254da61dddcd1ccceb5d7de941b
elhoucine20 <elhoucinenaitbrahim@gmail.com>
2026-03-11 10:17:35 +0000

    problem de merging

--- database/migrations/2026_03_09_161711_create_traitements_table.php
+++ database/migrations/2026_03_09_161711_create_traitements_table.php
@@ -13,10 +13,7 @@ return new class extends Migration
     {
         Schema::create('traitements', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('description');
-            $table->string('disease');
-            $table->integer('dosage');
             $table->timestamps();
         });
     }
